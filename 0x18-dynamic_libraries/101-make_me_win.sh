583c27ca65e532695e1521411329882860e22769
tagwa015 <tagwahassan15@gmail.com>
2023-12-11 17:06:43 -0800

    updat files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp http://github.com/tagwa015/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp http://github.com/tagwa015/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
