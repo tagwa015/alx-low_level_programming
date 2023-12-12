cc19c4f00ce3ceb566d4129dcc5f2e60a1e1d22e
tagwa015 <tagwahassan15@gmail.com>
2023-12-11 17:10:44 -0800

    updat files

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into object files
-gcc -c -Wall -Werror -Wextra -pedantic -fPIC *.c
-
-# Create the dynamic library
-gcc -shared -o liball.so *.o
-
-# Clean up the object files
-rm -f *.o
+#!/bin/bash
+gcc -fpic -c *.c
+gcc -shared *.o -o liball.so
